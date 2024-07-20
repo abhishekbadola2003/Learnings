e2229898d7ba80d805dd1aa669df08abc7a68ada
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-07-20 19:33:43 +0530

    learnings during node.js
    got to know about strings and it's methods.

---
+++ strings.js
@@ -0,0 +1,21 @@
+let namee = "hello world";
+// console.log(namee);
+
+//charAt is used to return character at a specified index.
+
+// name.charAt(4);
+// console.log(namee.charAt(4));
+
+// name.charAt(4,5);
+// console.log(namee.charAt(4,5));
+
+// //replace string characters
+// console.log(namee.replace('hello world','Namaste'));
+
+// //merge strings
+// const node = ' this is nodejs';
+// console.log(namee.concat(node));
+
+//remove whitespaces
+const ok = "    Hello    World    ";
+console.log(ok.trim());
