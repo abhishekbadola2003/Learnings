745e9e69a9d7ca5daaac4615ad9ea77070989196
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-07-22 13:28:25 +0530

    learnings in node.js
    learning objects in node.js

---
+++ object.js
@@ -0,0 +1,25 @@
+const person = {
+  firstname: "Abhishek",
+  lastname: "Badola",
+  role: "js developer",
+  Age: 21,
+  isemployed: true,
+  sayhello: (){
+    // let that = this;
+    // console.log("HELLO, My name is : ", that.firstname);
+    console.log("HELLO, My name is : ", person.firstname);
+  },
+  sayhi: function () {
+    let that = this;
+    console.log("I'm a ", this.role);
+  },
+};
+
+console.log(person.firstname);
+person.sayhello();
+person.sayhi();
+console.log(person.isemployed);
+
+
+// this was the window object thats why it is showing undefined 
+//converting this intp global object with the help of let that  
