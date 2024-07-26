21b738e534a3b9d1dcfce082d6bcae30ad4d3101
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-07-26 12:53:22 +0530

    Update methods.js

--- methods.js
+++ methods.js
@@ -12,3 +12,21 @@ takeOff(){
 
 alienShip.retreat();
 alienShip.takeOff();
+
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+  };
+  
+  // Write your code below
+  let greenEnergy=obj=>{
+    obj['Fuel Type']='avocado oil';
+  }
+  let remotelyDisable=obj=>{
+    obj.disabled=true;
+  }
+  greenEnergy(spaceship);
+  remotelyDisable(spaceship);
+  console.log(spaceship);
+  
+  
