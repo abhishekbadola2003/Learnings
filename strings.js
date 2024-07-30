6a01b22f1eac4b60db34a419891cf2d420ebd487
Abhishek-Badola <abhishekbadola28@gmail.com>
2024-07-30 10:25:35 +0530

    Learnings in javascript
    Got to know about strings and how to use them

--- strings.js
+++ strings.js
@@ -1,31 +1,23 @@
 let namee = "hello world";
-// console.log(namee);
+console.log(namee);
 
-//charAt is used to return character at a specified index.
+// charAt is used to return character at a specified index.
 
-// name.charAt(4);
-// console.log(namee.charAt(4));
+namee.charAt(4);
+console.log(namee.charAt(4));
 
-// name.charAt(4,5);
-// console.log(namee.charAt(4,5));
+namee.charAt(4,5);
+console.log(namee.charAt(4,5));
 
-// //replace string characters
-// console.log(namee.replace('hello world','Namaste'));
+//replace string characters
+console.log(namee.replace('hello world','Namaste'));
 
-// //merge strings
-// const node = ' this is nodejs';
-// console.log(namee.concat(node));
+//merge strings
+const node = ' this is nodejs';
+console.log(namee.concat(node));
 
-// //remove whitespaces
-// const ok = "    Hello    World    ";
-// console.log(ok.trim());
+//remove whitespaces
+const ok = "    Hello    World    ";
+console.log(ok.trim());
 
 console.log(namee.length);
-
-let fruits = ['apple','banana','mango'];
-
-fruits.push('orange');
-console.log(fruits);
-
-fruits.unshift('papaya');
-console.log(fruits);
